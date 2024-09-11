af01213bfc318a3913de2ff7cb2c034c5b879097
Yongwook <kyw9885@gmail.com>
2024-09-11 17:17:10 +0900

    [0911] 상속, 다형성

---
+++ chap09-polymorphism-lecture-sourse/src/main/java/com/ohgiraffers/section01/polymorphism/aaaaa.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.polymorphism;
+
+public class aaaaa {
+    public static void main(String[] args) {
+
+        int n = 10;
+        int num = 0;
+        int num1 = 0;
+
+        if (n % 2 == 0) {
+            for (int i = 0; i <= n; i++) {
+                num += i;
+            }
+            System.out.println(num);
+    }
+}
