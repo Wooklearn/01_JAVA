2a068bfebc1c5bc32209a9e3e07502c41826455a
Yongwook <kyw9885@gmail.com>
2024-09-13 18:15:42 +0900

    [0913] 콘솔게임 만들기

--- chap-Wook-MiniProject/src/main/java/miniproject/Application01.java
+++ chap-Wook-MiniProject/src/main/java/miniproject/Application01.java
@@ -1,39 +1,35 @@
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+package miniproject;
+
 import java.util.Scanner;
 
-public class Application {
+public class Application01 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+
         while (true) {
 
         System.out.println("게임을 시작합니다.");
+        System.out.println();
+//        System.out.println("현재 돈 : " + money);
         System.out.println("1. 작물 선택");
-        System.out.println("2. 작물 심기");
-        System.out.println("3. 작물 성장");
-        System.out.println("4. 작물 팔기");
+        System.out.println("2. 작물 성장 (가위바위보)");
+        System.out.println("3. 작물 팔기");
         System.out.println("0. 게임 설명");
         System.out.print("숫자를 입력하세요 : ");
         int num = sc.nextInt();
 
             switch (num) {
                 case 1:
-                    //작물 선택하는 기능
-                    Crops.selectCrop(sc);
-                    System.out.println("1. 작물을 선택하십시오.");
+                    System.out.println("1. 작물을 선택하세요.");
                     break;
                 case 2:
-                    System.out.println("2. 작물을 심습니다.");
+                    System.out.println("2. 작물에 물을 줍니다.");
                     break;
                 case 3:
-                    System.out.println("3. 작물에 물을 줍니다.");
-                    break;
-                case 4:
-                    System.out.println("4. 작물을 팝니다.");
+                    System.out.println("3. 작물을 팝니다.");
                     break;
                 case 0:
                     System.out.println("이 게임은 농장 게임입니다. 작물을 키우고 성장시킬 수 있으며, " +
@@ -43,7 +39,13 @@ public class Application {
                     System.out.println("잘못 누르셨습니다. 다시 입력해주세요.");
                     return;
             }
-            System.out.println("============================================================================================");
+            {
+//                if (money >= 1000) {
+//                    System.out.println("축하합니다!! 1000원을 모아서 게임이 종료됩니다.");
+//                    return;
+//                }
+            }
+            System.out.println("========================================================");
         }
     }
 }
