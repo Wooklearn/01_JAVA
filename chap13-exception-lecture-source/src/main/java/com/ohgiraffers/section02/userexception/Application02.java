97968ba8d5f595341856058aaabcd2c145f15d4f
Yongwook <kyw9885@gmail.com>
2024-09-20 14:36:51 +0900

    [0920] 예외처리

---
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application02.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section02.userexception;
+
+public class Application02 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/exception/MoenyNegativeException.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.userexception.exception;
+
+public class MoenyNegativeException extends NegativeException {
+
+    public MoenyNegativeException(String message) {
+        super(message);
+    }
+}
